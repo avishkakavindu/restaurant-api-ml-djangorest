12dd30c88c920ffa3b7f6489e886ee299d793876
avishkakavindu <avishkakavindud@gmail.com>
2021-12-01 14:29:14 +0530

    Initial commit

---
+++ api/admin.py
@@ -0,0 +1,80 @@
+from django.contrib import admin
+from api.models import *
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """ User Admin """
+    list_display = ['id', 'username', 'first_name', 'last_name', 'email']
+    search_fields = ['id', 'username', 'first_name', 'last_name', 'email', 'tp_no']
+
+
+class FoodInline(admin.StackedInline):
+    """ Inline view of foods belongs to menu """
+    model = Food
+    extra = 0
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    """ Menu admin """
+    list_display = ['id', 'name']
+    search_fields = ['id', 'name']
+    inlines = [FoodInline]
+
+
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    """ Food admin """
+    list_display = ['id', 'name', 'menu']
+    search_fields = ['id', 'name', 'menu']
+    list_filter = ['menu']
+
+
+class OrderedFoodInline(admin.StackedInline):
+    """ Inline view of foods belongs to a order """
+    model = OrderedFood
+    extra = 0
+
+
+class DeliveryDetailInline(admin.StackedInline):
+    """ Inline view of delivery details of a order """
+    model = DeliveryDetail
+    extra = 0
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        """ Deliverers only from user model """
+        if db_field.name == "deliverer":
+            kwargs["queryset"] = User.objects.filter(role=User.DELIVERY)
+        return super(DeliveryDetailInline, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """ Order admin """
+    list_display = ['order_id', 'user', 'order_type']
+    search_fields = ['order_id', 'user', 'order_type']
+    list_filter = ['order_type']
+    inlines = [OrderedFoodInline, DeliveryDetailInline]
+
+
+@admin.register(DeliveryDetail)
+class DeliveryDetail(admin.ModelAdmin):
+    """ DeliveryDetail Admin """
+
+    list_display = ['id', 'order', 'tel_no', 'current_location', 'deliverer']
+    search_fields = ['id', 'user', 'tel_no', 'deliverer', 'current_location']
+    
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        """ Deliverers only from user model """
+        if db_field.name == "deliverer":
+            kwargs["queryset"] = User.objects.filter(role=User.DELIVERY)
+        return super(DeliveryDetail, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    """ Table and Table reservation admin """
+    list_display = ['id', 'user', 'time', 'is_reserved']
+    search_fields = ['id', 'user']
+    list_filter = ['is_reserved']
---
+++ api/models.py
@@ -0,0 +1,100 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class User(AbstractUser):
+    """ Custom user model """
+
+    CUSTOMER = 0
+    DELIVERY = 1
+
+    USER_TYPES = [
+        (CUSTOMER, 'Customer'),
+        (DELIVERY, 'Delivery')
+    ]
+
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    tp_no = models.CharField(max_length=12)
+    role = models.PositiveSmallIntegerField(choices=USER_TYPES, default=CUSTOMER)
+
+    def __str__(self):
+        return self.username
+
+
+class Menu(models.Model):
+    """ Holds menu types information """
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Food(models.Model):
+    """ Holds food/meal information """
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount = models.DecimalField(max_digits=4, decimal_places=2,
+                                   validators=[MinValueValidator(0), MaxValueValidator(100)])
+    menu = models.ForeignKey(Menu, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    """ Holds order details """
+    DELIVERY = 0
+    TAKEAWAY = 1
+
+    ORDER_TYPES = [
+        (DELIVERY, 'Delivery'),
+        (TAKEAWAY, 'Takeaway')
+    ]
+
+    order_id = models.CharField(max_length=255, unique=True)
+    order_type = models.SmallIntegerField(choices=ORDER_TYPES, default=TAKEAWAY)
+    message = models.TextField()
+    ordered_food = models.ManyToManyField(
+        Food,
+        through='OrderedFood',
+        through_fields=['order', 'food']
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.order_id
+
+
+class OrderedFood(models.Model):
+    """ Holds foods belongs to a order """
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return '{}-{}'.format(self.order.order_id, self.food)
+
+
+class DeliveryDetail(models.Model):
+    """ Holds delivery details """
+    address = models.CharField(max_length=255)
+    tel_no = models.CharField(max_length=12)
+    current_location = models.CharField(max_length=255)
+    deliverer = models.ForeignKey(User, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.order.order_id
+
+
+class Table(models.Model):
+    """ Holds table and table reservation details """
+    time = models.DateTimeField()
+    is_reserved = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}-{}'.format(self.id, self.user)
