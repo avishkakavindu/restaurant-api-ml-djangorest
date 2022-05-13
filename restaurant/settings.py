9fdcbd85ffedd6c685a58f1f58d889e1f3155968
avishkakavindu <avishkakavindud@gmail.com>
2022-05-13 11:23:19 +0530

    AWS settings

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -178,15 +178,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# # AWS Configurations
-# AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-# AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-# AWS_QUERYSTRING_AUTH = False
-# # AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-#
-# # AWS_LOCATION = 'static'
-#
-# AWS_S3_FILE_OVERWRITE = False   # file with same name overwriting disable
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# AWS Configurations
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_QUERYSTRING_AUTH = False
+# AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+
+# AWS_LOCATION = 'static'
+
+AWS_S3_FILE_OVERWRITE = False   # file with same name overwriting disable
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
